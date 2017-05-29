4a4a58f2d272ada4ffc776452bbfcb0371a3fb3e
Brad Cannell <brad.cannell@gmail.com>
2017-05-29 14:31:31 -0500

    Added distribute_ever function

--- R scripts/functions.R
+++ R scripts/functions.R
@@ -24,6 +24,35 @@ count_ids <- function(x, ...) {
 
 
 
+# Distribute "have you ever" variables
+# If they say "no", then all previous observations should be no. If you've 
+# never had diabetes on day 100, then you didn't have diabetes on day 99.
+# If they say "Yes", then all future observations should be yes. If you have 
+# diabetes on day 100, then you have ever had diabetes on day 200.
+distribute_ever <- function(x, check_logic = TRUE, ...) {
+  if (check_logic) {
+    if (length(which(x == 1)) > 0 & length(which(x == 0)) > 0) {
+      if (min(which(x == 1)) < max(which(x == 0))) {
+        x                              <- 9                  # Set x to 9 if zero comes after 1
+      }
+    }
+  }
+  ones                                 <- which(x == 1)      # Get indices for 1's
+  if (length(ones) > 0) {
+    first_1_by_group                   <- min(which(x == 1)) # Index first 1 by group
+    x[seq_along(x) > first_1_by_group] <- 1                  # Set values of x at subsequent indices to 1
+  }
+  zeros                                <- which(x == 0)      # Get indices for 0's
+  if (length(zeros) > 0) {
+    last_0_by_group                    <- max(which(x == 0)) # Index last 0 by group
+    x[seq_along(x) < last_0_by_group]  <- 0                  # Set values of x at previous indices to 0
+  }
+  x
+}
+
+
+
+
 # Grab the closest value to baseline, that isn't baseline
 # Run on variables with high proportion of missing data, and longitudinal
 # information for that data, prior to MI

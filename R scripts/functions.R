94d1203ce4f2e0d1624b6fbb202b9d31532bcda0
Brad Cannell <brad.cannell@gmail.com>
2017-09-20 12:24:47 -0500

    Updated distribute_ever function
    No longer sets all values to 9 when there is a logical error. Just the values that come after the logical error (i.e., 0's after 1)

--- R scripts/functions.R
+++ R scripts/functions.R
@@ -29,23 +29,28 @@ count_ids <- function(x, ...) {
 # never had diabetes on day 100, then you didn't have diabetes on day 99.
 # If they say "Yes", then all future observations should be yes. If you have 
 # diabetes on day 100, then you have ever had diabetes on day 200.
+# Updated 2017-09-20: No longer sets all values to 9 when there is a logical 
+# error. Just the values that come after the logical error (i.e., 0's after 1)
 distribute_ever <- function(x, check_logic = TRUE, ...) {
   if (check_logic) {
-    if (length(which(x == 1)) > 0 & length(which(x == 0)) > 0) {
-      if (min(which(x == 1)) < max(which(x == 0))) {
-        x                              <- 9                  # Set x to 9 if zero comes after 1
+    if (length(which(x == 1)) > 0 & length(which(x == 0)) > 0) { # Check for 0's AND 1's
+      if (min(which(x == 1)) < max(which(x == 0))) {             # Check for 0 after 1
+        last_one                         <- max(which(x == 1))   # Index last "1"" by group
+        x[seq_along(x) > last_one]       <- 9                    # Set x (0) to 9 if zero comes after 1
+        x
       }
     }
-  }
-  ones                                 <- which(x == 1)      # Get indices for 1's
-  if (length(ones) > 0) {
-    first_1_by_group                   <- min(which(x == 1)) # Index first 1 by group
-    x[seq_along(x) > first_1_by_group] <- 1                  # Set values of x at subsequent indices to 1
-  }
-  zeros                                <- which(x == 0)      # Get indices for 0's
-  if (length(zeros) > 0) {
-    last_0_by_group                    <- max(which(x == 0)) # Index last 0 by group
-    x[seq_along(x) < last_0_by_group]  <- 0                  # Set values of x at previous indices to 0
+  } else {
+    ones                                 <- which(x == 1)        # Get indices for 1's
+    if (length(ones) > 0) {
+      first_1_by_group                   <- min(which(x == 1))   # Index first 1 by group
+      x[seq_along(x) > first_1_by_group] <- 1                    # Set values of x at subsequent indices to 1
+    }
+    zeros                                <- which(x == 0)        # Get indices for 0's
+    if (length(zeros) > 0) {
+      last_0_by_group                    <- max(which(x == 0))   # Index last 0 by group
+      x[seq_along(x) < last_0_by_group]  <- 0                    # Set values of x at previous indices to 0
+    }
   }
   x
 }
